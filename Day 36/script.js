2d1f7a72fc849a414d7e33ce93234fcc164133ba
koolkarniAtharva <katharva789@gmail.com>
2025-11-30 22:26:20 +0530

    Day 36

---
+++ Day 36/script.js
@@ -0,0 +1,6 @@
+// addEventListener("mousemove", function(e) {
+//     console.log(e.clientX , e.clientY);
+//     document.body.style.setProperty("--X",e.clientX + 'px');
+//     document.body.style.setProperty("--Y",e.clientY + 'px');
+// });
+
