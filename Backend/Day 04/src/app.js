ef69b0e0ab2e75b032eeac62d883d419e6750b2b
koolkarniAtharva <katharva789@gmail.com>
2026-02-02 17:47:56 +0530

    Day 04

---
+++ Backend/Day 04/src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+// POST/notes
+app.post("/notes",(req,res)=>{
+    notes.push(req.body);
+
+    res.status(201).json({
+        message:"Note created sucessfully"
+    })
+
+})
+
+// GET/notes
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        notes:notes
+    })
+})
+
+//DELETE /notes/:index
+
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index]
+
+    res.status(204).json({
+        message:"Note Deleted Successfully"
+    })
+})
+
+// PATCH /notes/:index
+
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description = req.body.description
+
+    res.status(200).json({
+        message: "Note Updated successfully"
+    })
+})
+
+module.exports = app;
