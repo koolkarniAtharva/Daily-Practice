096348eeb3ffb1ccc3e2cdf757805f40c7cbd0ee
koolkarniAtharva <katharva789@gmail.com>
2026-02-03 20:14:07 +0530

    Day 05

--- Backend/Day 05/server.js
+++ Backend/Day 05/server.js
@@ -3,7 +3,7 @@ const app = require("./src/app");
 const mongoose = require("mongoose");
 
 function connectToDb(){
-    mongoose.connect("mongodb+srv://evilofheaven11419_db_user:5tHEjb3KTGf3MZl4@cluster0.ocxjnyg.mongodb.net/day-5")
+    mongoose.connect("")
     .then(()=>{
         console.log("Connected to Database")
     })
