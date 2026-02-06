518d884929c9e9633f2bcf5bf27403b55d059609
koolkarniAtharva <katharva789@gmail.com>
2026-02-06 12:12:44 +0530

    Backend

---
+++ Backend/Day 56/backend/src/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { models } = require("mongoose");
+const noteModel = require("./models/note.models");
+
+const app = express()
+app.use(express.json());
+//POST api/notes
+
+app.post('/api/notes', async (req,res)=>{
+    const {title,description} = req.body ;
+    const note = await  noteModel.create({
+        title,description
+    })
+    res.status(201).json({
+        message:"Note Created Successfully",
+        note
+    });
+})
+
+//GET all the notes in DB
+app.get("/api/notes",async(req,res)=>{
+    const note = await noteModel.find()
+
+    res.status(200).json({
+        message:"Note fetched sucessfully",
+        note
+    })
+})
+
+
+//DELETE /api/notes
+app.delete("/api/notes/:id", async (req,res)=>{
+    const id = req.params.id
+    
+    const note = await noteModel.findByIdAndDelete(id)
+    
+    res.status(200).json({
+        message:"note Deletd sucessfully",
+        note
+    })
+
+})
+
+//Update /api/notes
+app.patch("/api/notes/:id", async(req,res)=>{
+    const id = req.params.id
+    const {description} = req.body
+    await noteModel.findByIdAndUpdate(id,{description})
+    
+    res.status(200).json({
+        message:"Note Updated sucessfully"
+    })
+})
+
+module.exports = app;
