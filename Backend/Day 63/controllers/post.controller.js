30763c6848656ddc336df2e3b79b8d9f81268e83
koolkarniAtharva <katharva789@gmail.com>
2026-03-29 22:37:45 +0530

    Day 63

---
+++ Backend/Day 63/controllers/post.controller.js
@@ -0,0 +1,81 @@
+const postModel = require("../models/post.model");
+const Imagekit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken")
+
+const imagekit = new Imagekit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+
+async function createPostController(req, res) {
+   
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName: "Test"
+    });
+
+
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: req.user.id
+    })
+
+    res.status(201).json({
+        message: "post created sucssesfully .",
+        post
+    })
+
+}
+
+async function getPostController(req, res) {
+    
+    const userId = req.user.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "Posts Fetched Sucessfully ",
+        posts
+    })
+
+
+}
+
+async function getPostDetails(req, res) {
+
+    const userId = req.user.id
+    const postId = req.params.postId
+
+
+    const post = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message:"Post not Found"
+        })
+    }
+
+    const isvalidUser = post.user.toString() === userId
+
+    if(!isvalidUser){
+        return res.status(403).json({
+            message:"Forbidden Content"
+        })
+    }
+
+    return res.status(200).json({
+        message:"Post Fetched Successfully",
+        post    
+    })
+
+}
+
+module.exports = {
+    createPostController,
+    getPostController,
+    getPostDetails
+};
---
+++ Backend/Day 63/models/follow.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const followSchema = new mongoose.Schema({
+    follower:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:[true,"Follower is Required"]
+    },
+    followee:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:[true,"Followee is Required"]  
+    }
+},{
+    timestamps:true
+})
+
+
+const followModel = mongoose.model("follows",followSchema)
+
+module.exports = followModel
